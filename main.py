cf4436eabeca15e44ed91a9073a1a2506f457413
Omar29-long <omarzeroualpro@gmail.com>
2026-04-06 16:34:20 +0200

    feat: integrate MLflow model registry and add model registration script

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ from pydantic import BaseModel
 import pandas as pd
 import numpy as np
 import mlflow
+from functools import lru_cache
 
 
-app = FastAPI(title="Credit Score API", description="Predicts the risk of default for a client")
+app = FastAPI(
+    title="Credit Score API",
+    description="Predicts the risk of default for a client"
+)
 
-# 1. Loading model from mlflow
-model = mlflow.sklearn.load_model("models:/best_credit_loan_model/Production")
-preprocessor = mlflow.sklearn.load_model("models:/credit_loan_preprocessor/1")
 
-# 2. Define the input data model
+@lru_cache
+def get_model():
+    return mlflow.sklearn.load_model("models:/best_credit_loan_model/1")
+
+
+@lru_cache
+def get_preprocessor():
+    return mlflow.sklearn.load_model("models:/credit_loan_preprocessor/1")
+
+
 class ClientData(BaseModel):
     credit_lines_outstanding: int
     loan_amt_outstanding: float
@@ -20,23 +30,24 @@ class ClientData(BaseModel):
     years_employed: int
     fico_score: int
 
+
 @app.get("/")
 def home():
     return {"message": "Credit Scoring API is operational. Go to /docs to test."}
 
+
 @app.post("/predict")
 def predict(client: ClientData):
-    # a. Convert the received JSON into a DataFrame
     input_df = pd.DataFrame([client.model_dump()])
-    
-    # b. Apply the preprocessor
+
+    preprocessor = get_preprocessor()
+    model = get_model()
+
     X_processed = preprocessor.transform(input_df)
 
-    # c. Prediction
     probability = model.predict_proba(X_processed)[0][1]
     prediction = int(model.predict(X_processed)[0])
 
-    # d. Return the result
     return {
         "status": "success",
         "default_probability": round(float(probability), 4),
